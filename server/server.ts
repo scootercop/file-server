41c791c1850733a2fb87e8c28a264f49b19a5c4e
Naval Rishi <narishi@microsoft.com>
2021-06-11 20:57:36 +0530

    clusters added

--- server/server.ts
+++ server/server.ts
@@ -4,38 +4,54 @@ import express = require("express");
 import { IncomingMessage, ServerResponse } from "http";
 import { ServerStore } from "./serverStore";
 import * as path from "path";
+import cluster from "cluster";
+import os from "os";
 
-const cliArguments = process.argv.slice(2);
-const rootFolder = cliArguments[0];
-const username = cliArguments[1];
-const password = cliArguments[2];
-const httpServer = express();
-httpServer.use(Logger);
-httpServer.get(
-  "/FSD",
-  function (
-    request: IncomingMessage,
-    response: ServerResponse,
-    next: Function
-  ) {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(ServerStore.fileFolderStructure));
-    next();
+const numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Primary ${process.pid} is running`);
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-);
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const cliArguments = process.argv.slice(2);
+  const rootFolder = cliArguments[0];
+  const username = cliArguments[1];
+  const password = cliArguments[2];
+  const httpServer = express();
+  httpServer.use(Logger);
+  httpServer.get(
+    "/FSD",
+    function (
+      request: IncomingMessage,
+      response: ServerResponse,
+      next: Function
+    ) {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(ServerStore.fileFolderStructure));
+      next();
+    }
+  );
 
-httpServer.use(express.static(path.join(__dirname, "..")));
-httpServer.listen(8001);
+  httpServer.use(express.static(path.join(__dirname, "..")));
+  httpServer.listen(8001);
 
-const dataFileServer = hs.createServer({
-  root: rootFolder,
-  proxy: "http://127.0.0.1:8001/",
-  showDir: "false",
-  autoIndex: "false",
-  username: username,
-  password: password,
-});
+  const dataFileServer = hs.createServer({
+    root: rootFolder,
+    proxy: "http://127.0.0.1:8001/",
+    showDir: "false",
+    autoIndex: "false",
+    username: username,
+    password: password,
+  });
 
-dataFileServer.listen(8000);
+  dataFileServer.listen(8000);
 
-console.log("server started");
+  console.log("server started");
+}
